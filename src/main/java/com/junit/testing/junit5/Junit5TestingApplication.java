8f4feb0524b127ab5d27b365e1afa3d09bfdab83
Jayanth <110449215+jayanth336@users.noreply.github.com>
2024-02-19 13:03:41 +0530

    Add files via upload

---
+++ src/main/java/com/junit/testing/junit5/Junit5TestingApplication.java
@@ -0,0 +1,13 @@
+package com.junit.testing.junit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5TestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junit/testing/junit5/Junit5TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit.testing.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
